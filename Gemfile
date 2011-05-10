44571613d62a216120c547696beb52e262b7ff3a
Leland Husband <leland.husband@gmail.com>
2011-05-10 00:47:47 -0500

    First commit.

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.beta1'
+
+# Asset template engines
+gem 'json'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+
+group :production do
+  gem 'therubyracer-heroku', '0.8.1.pre3'
+  gem 'pg', :require => 'pg'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gunnertalkapp::Application.config.secret_token = 'f258fae4f17baf10cf1110d450c82fbc287a57c238eca81aef5b3a3e6f08f36d53783a32b8af0a1f5c085370e03489b270a1a33da1bcd64e2645ccadd8d3cb6a'
